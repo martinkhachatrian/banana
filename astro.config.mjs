e1f9865a8bf8e08614ff4795afab56256cc078c1
martin <martin.devcraft@gmail.com>
2024-10-23 20:03:59 +0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	// site: 'https://example.com',
+	base: '/banana/',
 	integrations: [mdx(), sitemap()],
 });

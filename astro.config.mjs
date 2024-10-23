499a4b9e88a2bf54082a52de7d0e408838179385
martin <martin.devcraft@gmail.com>
2024-10-23 20:21:57 +0400

    add nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	// site: 'https://example.com',
+	site: 'https://martinkhachatrian.github.io',
 	base: '/banana/',
 	integrations: [mdx(), sitemap()],
 });
